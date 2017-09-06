1c674f34f191b441d77b653e75a79d2fa343e129
root <root@NIKI.localdomain>
2017-09-07 00:04:51 +0530

    Chat App completed

---
+++ server.js
@@ -0,0 +1,30 @@
+//server.js
+
+var express = require('express');
+var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io')(server);
+
+
+app.get('/',function(req,res,next){
+	res.sendFile(__dirname + '/public/index.html');
+});
+
+app.use(express.static('public'));
+
+io.on('connection',function(client){
+	console.log('client connected');
+	client.on('join',function(data){
+	 console.log(data);
+});
+	client.on('messages',function(data){
+	client.emit('thread',data);
+	console.log(data);
+	client.broadcast.emit('thread',data);
+	});
+});
+
+
+
+
+server.listen(7777);
